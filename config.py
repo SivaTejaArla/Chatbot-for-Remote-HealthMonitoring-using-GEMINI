6845982a13a8cf2870ab803a77ecbc4858a0940a
SivaTejaArla <sivatejaarla@gmail.com>
2025-04-24 12:55:57 +0530

    .

--- config.py
+++ config.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 apiKey=""
 
-=======
-apiKey="AIzaSyD9jPbIPfyP8lUlWoJur-OXLkylxcx0DP8"
-
->>>>>>> ff8045e (modified)
--- sample.py
+++ sample.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 import result
-=======
-import result
->>>>>>> ff8045e (modified)
 print("nn")

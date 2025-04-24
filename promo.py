a39cda8ac268df04274cbdfbe2e33e37bf7ae2ae
SivaTejaArla <sivatejaarla@gmail.com>
2025-04-24 13:01:45 +0530

    .

--- promo.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyD9jPbIPfyP8lUlWoJur-OXLkylxcx0DP8")
-
-dat = "who are you"
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents= dat
-)
-print(response.text)
